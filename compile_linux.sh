aa46a5588c5b8351d6a256bf9adad0201e13c34b
onepiecefreak3 <alberteinstein1980@gmx.de>
2019-06-13 01:21:16 +0200

    Added build directory check to linux build script

--- compile_linux.sh
+++ compile_linux.sh
@@ -1,2 +1,3 @@
+if [ ! -d "build" ]; then mkdir "build"; fi
 g++ -D__Unix -fPIC -m32 -shared -o build/ukkonen32.so UkkonenSuffixTree/UkkonenExport.cpp UkkonenSuffixTree/SuffixTree.cpp UkkonenSuffixTree/SuffixTreeNode.cpp
 g++ -D__Unix -fPIC -shared -o build/ukkonen64.so UkkonenSuffixTree/UkkonenExport.cpp UkkonenSuffixTree/SuffixTree.cpp UkkonenSuffixTree/SuffixTreeNode.cpp
